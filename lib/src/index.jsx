e17c84eb98d01ead85230a711049c980e02c35f8
matheusfrancisco <matheusmachadoufsc@gmail.com>
2022-09-11 19:16:30 -0300

    build react components to use

---
+++ lib/src/index.jsx
@@ -0,0 +1,13 @@
+import { Button } from './components/Button';
+import { Container } from './components/Container';
+import { AuthLayout } from './components/AuthLayout';
+import { Fields } from './components/Fields';
+import { Header } from './components/Header';
+import { Logo } from './components/Logo';
+import { Hero } from './components/Hero';
+import { Pricing } from './components/Pricing';
+import { NavLink } from './components/NavLink';
+import { Faqs } from './components/Faqs';
+
+
+export { Button, Container, AuthLayout, Faqs, Fields, Header, Logo, Hero, Pricing, NavLink};
