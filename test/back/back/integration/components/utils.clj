b5fdbe2498b438dd94db4ccf5d6e926397614860
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-06-20 21:12:56 -0300

    add: database test

---
+++ test/back/back/integration/components/utils.clj
@@ -0,0 +1,18 @@
+(ns back.integration.components.utils
+  (:require [com.stuartsierra.component :as component]
+            [back.components.logs :as logs]
+            [back.components.migrations :as migrations]
+            [pg-embedded-clj.core :as pg-emb]))
+
+(defn start-system!
+  [system-start-fn]
+  (fn []
+    (logs/setup :info :auto)
+    (pg-emb/init-pg)
+    (migrations/migrate (migrations/configuration-with-db))
+    (system-start-fn)))
+
+(defn stop-system!
+  [system]
+  (component/stop-system system)
+  (pg-emb/halt-pg!))
