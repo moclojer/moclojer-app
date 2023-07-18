47996a7292ca3a45c54e9430f7e095c0264f141b
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-07-17 22:37:25 -0300

    edit: reuse dashboard structure

--- src/front/app/core.cljs
+++ src/front/app/core.cljs
@@ -1,23 +1,24 @@
 (ns front.app.core
-  (:require
-   ["flowbite"]
-   ["react-dom/client" :as rdom]
-   [front.app.auth.db]
-   [front.app.auth.events]
-   [front.app.auth.subs]
-   [front.app.dashboard.subs]
-   [front.app.dashboard.events]
-   [front.app.components.footer :refer [footer-component]]
-   [front.app.lib :refer [defnc]]
-   [front.app.routes.bookmarks :as routes.bookmarks]
-   [front.app.routes.core :as routes]
-   [front.app.routes.events]
-   [front.app.routes.subs]
-   [helix.core :refer [$]]
-   [helix.dom :as d]
-   [helix.hooks :as hooks]
-   [refx.alpha :as refx]
-   [reitit.frontend.easy :as rfe]))
+  (:require ["flowbite"]
+            ["react-dom/client" :as rdom]
+            [front.app.auth.db]
+            [front.app.auth.events]
+            [front.app.auth.subs]
+            [front.app.components.container :refer [container]]
+            [front.app.components.footer :refer [footer-component]]
+            [front.app.dashboard.components :refer [aside nav-bar]]
+            [front.app.dashboard.events]
+            [front.app.dashboard.subs]
+            [front.app.lib :refer [defnc]]
+            [front.app.routes.bookmarks :as routes.bookmarks]
+            [front.app.routes.core :as routes]
+            [front.app.routes.events]
+            [front.app.routes.subs]
+            [helix.core :refer [$]]
+            [helix.dom :as d]
+            [helix.hooks :as hooks]
+            [refx.alpha :as refx]
+            [reitit.frontend.easy :as rfe]))
 
 (def default-db
   {:current-route nil
@@ -42,7 +43,26 @@
   (let [route-data (:data match)
         view (:view route-data)
         user (refx/use-sub [:app.auth/current-user])]
-    ($ view {:match match})))
+
+    (let [user (refx/use-sub [:app.auth/current-user])
+          [toggle-sidebar set-toggle] (hooks/use-state false)
+          user-data (-> user :user)]
+
+      (d/body
+       {:class-name "bg-gray-50 dark:bg-gray-800"}
+       ($ nav-bar {:set-toggle set-toggle
+                   :toggle-sidebar toggle-sidebar
+                   :user-data user-data})
+
+       ($ aside {:is-sidebar-toogle? toggle-sidebar
+                 :set-toggle set-toggle})
+
+       (d/div {:class-name "hidden fixed inset-0 z-10 bg-gray-900/50 dark:bg-gray-900/90"
+               :id "sidebarBackdrop"})
+       ($ container
+          {:is-sidebar-toogle? toggle-sidebar}
+          ($ view {:match match
+                   :is-sidebar-toogle? toggle-sidebar}))))))
 
 (defnc routing []
   (let [current-route (refx/use-sub [:app.routes/current-route])
