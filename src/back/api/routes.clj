fded9b089e7169231924659c29d727cc93069206
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-08-11 18:33:55 -0300

    rebase: fix conflicts

--- src/back/api/routes.clj
+++ src/back/api/routes.clj
@@ -4,8 +4,7 @@
    [back.api.schemas.wire-in :as schemas.wire-in]
    [back.api.schemas.wire-out :as schemas.wire-out]
    [back.api.healthcheck :as healthcheck]
-   [reitit.swagger :as swagger]
-   [reitit.ring.middleware.multipart :as multipart]))
+   [reitit.swagger :as swagger]))
 
 (def routes
   [["/swagger.json"
@@ -29,21 +28,21 @@
    ["/login/auth"
     {:swagger {:tags ["login"]}
      :post {:summary "Login supabase retrieve user"
-            :parameters {:body auth.schemas.wire-in/AuthLogin}
-            :responses {201 {:body {:user auth.schemas.wire-out/User}}}
-            :handler auth.ports.http-in/handler-create-user!}}]
+            :parameters {:body schemas.wire-in/AuthLogin}
+            :responses {201 {:body {:user schemas.wire-out/User}}}
+            :handler ports.http-in/handler-create-user!}}]
 
    ["/user/:id"
     {:swagger {:tags ["login"]}
      :post {:summary "Update user"
             :parameters {:path {:id uuid?}
                          :body {:username string?}}
-            :responses {200 {:body {:user auth.schemas.wire-out/User}}}
-            :handler auth.ports.http-in/edit-user!}
+            :responses {200 {:body {:user schemas.wire-out/User}}}
+            :handler ports.http-in/edit-user!}
      :get {:summary "Retrieve user"
            :parameters {:path {:id uuid?}}
-           :responses {200 {:body {:user auth.schemas.wire-out/User}}}
-           :handler auth.ports.http-in/get-user-by-id}}]
+           :responses {200 {:body {:user schemas.wire-out/User}}}
+           :handler ports.http-in/get-user-by-id}}]
 
    ["/mocks"
     {:swagger {:tags ["mocks"]}
