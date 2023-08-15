4ba7870db9cde62c2b04f4c91ae01cc160af7743
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-08-14 22:11:28 -0300

    add connect to mock get

--- src/front/app/dashboard/events.cljs
+++ src/front/app/dashboard/events.cljs
@@ -18,3 +18,36 @@
      {:db  (assoc db
                   :is-modal-open? (not toggle))})))
 
+(refx/reg-event-fx
+ :app.dashboard/get-mocks-success
+ (fn [{db :db} [_ response]]
+   (prn :get-mocks-success response)
+   {:db (assoc db
+               :mocks
+               [{:type :personal :name "my" :apis [{:enable false :url "test01.avelino.moclojer.com" :id 1}
+                                                   {:enable true  :url "test02.avelino.moclojer.com" :id 2}]}
+                {:type :org :name "cljazz" :apis [{:enable false :url "test01.avelino.moclojer.com" :id 1}
+                                                  {:enable true  :url "test02.avelino.moclojer.com" :id 2}]}]
+               #_(-> response :body))}))
+
+(refx/reg-event-fx
+ :app.dashboard/get-mocks-failure
+ (fn [{db :db} [_ response]]
+   (prn :get-mocks-failure response)
+   {:db (assoc db
+               :error-fetch-mocks false
+               :mocks
+               [{:type :personal :name "my" :apis [{:enable false :url "test01.avelino.moclojer.com" :id 1}
+                                                   {:enable true  :url "test02.avelino.moclojer.com" :id 2}]}
+                {:type :org :name "cljazz" :apis [{:enable false :url "test01.avelino.moclojer.com" :id 1}
+                                                  {:enable true  :url "test02.avelino.moclojer.com" :id 2}]}])}))
+
+(refx/reg-event-fx
+ :app.dashboard/get-mocks
+ (fn [{db :db} [_ user]]
+   (prn :user-get-mocks user)
+   {:http {:url "/api/mocks"
+           :method :get
+           :on-success [:app.dashboard/get-mocks-success]
+           :on-failure [:app.dashboard/get-mocks-failure]}
+    :db db}))
