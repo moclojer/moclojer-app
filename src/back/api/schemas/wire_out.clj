91547867356ef4c1f364426c1644ce80bd6e6265
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-08-30 09:14:00 -0300

    remove some comments

--- src/back/api/schemas/wire_out.clj
+++ src/back/api/schemas/wire_out.clj
@@ -27,17 +27,15 @@
                        :email "email"})
 
   (require '[malli.core :as m])
-  (m/validate User user-example) ; => true
+  (m/validate User user-example)
 
-  (m/validate User user-example-2) ; => true 
+  (m/validate User user-example-2)
 
   (def mock  {:id #uuid "19b3a076-8fd6-4b46-94fa-3650419b2a8f",
               :user-id #uuid "cd989358-af38-4a2f-a1a1-88096aa425a7",
               :wildcard "test"
               :subdomain "chico",
               :enabled true})
-  
-  (m/validate Mock mock) ; => true
-  ;
-  )
+
+  (m/validate Mock mock))
 
