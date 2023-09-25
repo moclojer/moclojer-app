7d0efcdb7f17e512824a35c770cfb5c0df7724e8
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-09-25 19:50:23 -0300

    add: endpoint to check if username is available

---
+++ test/back/back/integration/api/db/get_username_test.clj
@@ -0,0 +1,45 @@
+(ns back.integration.api.db.get-username-test
+  (:require [back.api.db.customers :as db.customers]
+            [back.components.config :as config]
+            [back.components.database :as database]
+            [state-flow.assertions.matcher-combinators :refer [match?]]
+            [back.integration.components.utils :as utils]
+            [com.stuartsierra.component :as component]
+            [state-flow.api :refer [defflow]]
+            [state-flow.core :refer [flow]]
+            [state-flow.state :as state]))
+
+(defn- create-and-start-components! []
+  (component/start-system
+   (component/system-map
+    :config (config/new-config)
+    :database (component/using (database/new-database)
+                               [:config]))))
+
+(defflow flow-db-get-username
+  {:init (utils/start-system! create-and-start-components!)
+   :cleanup utils/stop-system!
+   :fail-fast? true}
+
+  (flow "get username"
+
+    [database (state-flow.api/get-state :database)]
+
+    (state/invoke
+     #(db.customers/insert!
+       {:customer/uuid #uuid "cd989358-af38-4a2f-a1a1-88096aa425a7"
+        :customer/email "test@gmail.com"
+        :customer/external-uuid #uuid "dcff4c06-1c9e-4abb-a49b-438e1869ec5b"
+        :customer/username "chico"}
+       database))
+
+    (flow "get by username"
+      (match? {:customer/uuid #uuid "cd989358-af38-4a2f-a1a1-88096aa425a7"
+               :customer/email "test@gmail.com"
+               :customer/username "chico"
+               :customer/external_uuid #uuid "dcff4c06-1c9e-4abb-a49b-438e1869ec5b"}
+              (db.customers/get-by-username "chico" database)))
+
+    (flow "get by username"
+      (match? nil
+              (db.customers/get-by-username "chicoo" database)))))
---
+++ test/back/back/integration/api/username_is_available_test.clj
@@ -0,0 +1,64 @@
+(ns back.integration.api.username-is-available-test
+  (:require [back.api.db.customers :as db.customers]
+            [back.api.routes :as routes]
+            [back.components.config :as config]
+            [back.components.database :as database]
+            [back.components.http :as http]
+            [back.components.router :as router]
+            [back.components.webserver :as webserver]
+            [back.integration.api.helpers :as helpers]
+            [back.integration.components.utils :as utils]
+            [com.stuartsierra.component :as component]
+            [matcher-combinators.matchers :as matchers]
+            [state-flow.api :refer [defflow] :as flow]
+            [state-flow.assertions.matcher-combinators :refer [match?]]
+            [state-flow.cljtest]
+            [state-flow.core :refer [flow]]
+            [state-flow.state :as state]))
+
+(defn- create-and-start-components! []
+  (component/start-system
+   (component/system-map
+    :config (config/new-config)
+    :http (http/new-http-mock {})
+    :router (router/new-router routes/routes)
+    :database (component/using (database/new-database)
+                               [:config])
+    :webserver (component/using (webserver/new-webserver)
+                                [:config :http :router :database]))))
+
+(def token "Beare eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkY2ZmNGMwNi0xYzllLTRhYmItYTQ5Yi00MzhlMTg2OWVjNWIifQ.Gd42MG5EQCVvQwsvlhRQWHuEr-BBo4GB7Pd9di8w_No")
+
+(defflow
+  flow-check-if-username-is-available
+  {:init (utils/start-system! create-and-start-components!)
+   :cleanup utils/stop-system!
+   :fail-fast? true}
+  (flow "setup a customer"
+
+    [database (state-flow.api/get-state :database)]
+
+    (state/invoke
+     #(db.customers/insert! {:customer/uuid #uuid "cd989358-af38-4a2f-a1a1-88096aa425a7"
+                             :customer/email "test@gmail.com"
+                             :customer/username "chico"
+                             :customer/external-uuid #uuid "dcff4c06-1c9e-4abb-a49b-438e1869ec5b"}
+                            database))
+
+    (flow "check if username is available"
+      [resp (helpers/request! {:method :get
+                               :headers {"authorization" token}
+                               :uri "/user/username/chico"})]
+      (match?
+       (matchers/embeds {:status 200
+                         :body {:available false}})
+       resp))
+
+    (flow "username is available"
+      [resp (helpers/request! {:method :get
+                               :headers {"authorization" token}
+                               :uri "/user/username/test"})]
+      (match?
+       (matchers/embeds {:status 200
+                         :body {:available true}})
+       resp))))
