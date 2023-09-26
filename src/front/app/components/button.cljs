31031a9a803973eaecf2cb6abd123f524c1ddc75
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-09-25 21:55:11 -0300

    add: frontend when saving user

--- src/front/app/components/button.cljs
+++ src/front/app/components/button.cljs
@@ -15,9 +15,11 @@
   {:solid {:slate "bg-slate-900 text-white hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900 ",
            :blue "bg-blue-600 text-white hover:text-slate-100 hover:bg-blue-500 active:bg-blue-800 active:text-blue-100 focus-visible:outline-blue-600 ",
            :white "bg-white text-slate-900 hover:bg-blue-50 active:bg-blue-200 active:text-slate-600 focus-visible:outline-white "},
-   :solid-blue {:pink " login-button"}
+   :solid-blue {:pink " login-button"
+                :grey " login-button-block"}
    :outline {:slate "ring-slate-200 text-slate-700 hover:text-slate-900 hover:ring-slate-300 active:bg-slate-100 active:text-slate-600 focus-visible:outline-blue-600 focus-visible:ring-slate-300 ",
              :pink " login-button"
+             :grey " login-button-block"
              :white "ring-slate-700 text-white hover:ring-slate-500 active:ring-slate-700 active:text-slate-400 focus-visible:outline-white "}})
 
 (defn props->classes
