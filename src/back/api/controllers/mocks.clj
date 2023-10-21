10ac18fb930fb16f06140f082130ab75d4cb5b7c
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-10-21 19:26:14 -0300

    add test to check producer was called

--- src/back/api/controllers/mocks.clj
+++ src/back/api/controllers/mocks.clj
@@ -1,11 +1,12 @@
 (ns back.api.controllers.mocks
-  (:require
-   [back.api.adapters.mocks :as adapter.mocks]
-   [back.api.db.mocks :as db.mocks]
-   [back.api.logic.mocks :as logic.mocks]))
+  (:require [back.api.adapters.mocks :as adapter.mocks]
+            [back.api.db.mocks :as db.mocks]
+            [back.api.logic.mocks :as logic.mocks]
+            [back.api.ports.producers :as ports.producers]))
 
 (defn create-mock!
-  [user-id {:keys [subdomain content wildcard]} {:keys [database]}]
+  [user-id {:keys [subdomain content wildcard]}
+   {:keys [database]}]
   (-> (logic.mocks/create {:subdomain subdomain
                            :user-id user-id
                            :wildcard wildcard
@@ -14,11 +15,12 @@
       (adapter.mocks/->wire)))
 
 (defn update-mock!
-  [id content {:keys [database]}]
+  [id content {:keys [database publisher]}]
   (-> (db.mocks/get-mock-by-id id database)
       (logic.mocks/update {:content content})
       (db.mocks/update! database)
-      (adapter.mocks/->wire)))
+      (adapter.mocks/->wire)
+      (ports.producers/mock-updated publisher)))
 
 (defn get-mocks
   [filters {:keys [database]}]
@@ -38,3 +40,4 @@
       logic.mocks/unpublish
       (db.mocks/update! database))
   true)
+
