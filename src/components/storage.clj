7a6ec0977f47afa947a5a4ee4e635355971e8e14
matheusfrancisco <matheusmachadoufsc@gmail.com>
2023-11-08 08:42:09 -0300

    add config profile to use localstack

--- src/components/storage.clj
+++ src/components/storage.clj
@@ -1,14 +1,34 @@
 (ns components.storage
-  (:require [cognitect.aws.client.api :as aws]
+  (:require [clojure.java.io :as io]
+            [cognitect.aws.client.api :as aws]
             [cognitect.aws.credentials :as cred]
             [com.stuartsierra.component :as component]
-            [components.config :as config]))
+            [components.config :as config]
+            [components.logs :as logs]))
 
 (defprotocol IStorage
   (get-bucket [this bucket-name])
   (list-buckets [this])
   (create-bucket! [this bucket-name])
-  (upload! [this bucket-name key value encoding]))
+  (delete-bucket! [this bucket-name])
+  (delete-file! [this bucket-name filename])
+  (get-file [this bucket-name filename])
+  (list-files [this bucket-name])
+  (upload! [this bucket-name key value]))
+
+(defn assoc-if [m k v]
+  (if v
+    (assoc m k v)
+    m))
+
+(defn ^:private endpoint-override [{:keys [config]}]
+  (let [port (-> config :config :storage :port)
+        config-override (->
+                         {:protocol :http
+                          :hostname (-> config :storage :host)}
+                         (assoc-if :port port))]
+    (clojure.pprint/pprint config-override)
+    config-override))
 
 (defrecord Storage [config]
   component/Lifecycle
@@ -19,8 +39,7 @@
                           :credentials-provider (cred/basic-credentials-provider
                                                  {:access-key-id (-> config :config :storage :access-key-id)
                                                   :secret-access-key (-> config :config :storage :secret-access-key)})
-                          :endpoint-override {:protocol :https
-                                              :hostname (-> config :config :storage :host)}})]
+                          :endpoint-override (endpoint-override config)})]
 
       (assoc this :storage s3)))
   (stop [this]
@@ -45,17 +64,58 @@
         (aws/invoke {:op :CreateBucket
                      :request {:Bucket bucket-name}})))
 
-  (upload! [this bucket-name key value encoding]
+  (list-files [this bucket-name]
+    (-> (:storage this)
+        (aws/invoke {:op :ListObjects
+                     :request {:Bucket bucket-name}})
+        :Contents))
+
+  (upload! [this bucket-name filename value]
+    (logs/log :info :upload filename value)
     (-> (-> this :storage)
         (aws/invoke {:op :PutObject
                      :request {:Bucket bucket-name
-                               :Key key
-                               :Body (encoding value)}}))))
+                               :Key filename
+                               :Body (.getBytes value)}})))
+
+  (get-file [this bucket-name filename]
+    (-> (:storage this)
+        (aws/invoke {:op :GetObject
+                     :request {:Bucket bucket-name
+                               :Key filename}})
+        :Body))
+
+  (delete-file! [this bucket-name filename]
+    (-> (-> this :storage)
+        (aws/invoke {:op :DeleteObject
+                     :request {:Bucket bucket-name
+                               :Key filename}})))
+
+  (delete-bucket! [this bucket-name]
+    (-> (-> this :storage)
+        (aws/invoke {:op :DeleteBucket
+                     :request {:Bucket bucket-name}}))))
 
 (defn new-storage []
   (map->Storage {}))
 
 (comment
+  (def yml "
+- endpoint:
+    # Note: the method could be omitted because GET is the default
+    method: GET
+    path: /hello/:username
+    response:
+      # Note: the status could be omitted because 200 is the default
+      status: 200
+      headers:
+        Content-Type: application/json
+      # Note: the body will receive the value passed in the url using the
+      # :username placeholder
+      body: >
+        {
+          \"hello\": \"{{path-params.username}}!\"
+        }")
 
   (def system-map
     (component/system-map
@@ -64,16 +124,27 @@
                (new-storage) [:config])))
 
   (def system-atom (atom {}))
+
   (->> system-map
        component/start
        (reset! system-atom))
+
   (def storage
     (:storage @system-atom))
 
   (get-bucket storage "moclojer")
 
   (list-buckets storage)
-  #_(create-bucket! storage "moclojer")
+
+  (list-files storage "moclojer")
+
+  (slurp (io/reader (get-file storage "moclojer" "test.yml")))
+
+  (upload! storage "moclojer" "test.yml" yml)
+
+  #_(list-buckets storage)
+  (create-bucket! storage "moclojer")
+  #_(delete-file! storage "moclojer" "test.yml")
   ;
   )
 
--- src/yaml_generator/ports/workers.clj
+++ src/yaml_generator/ports/workers.clj
@@ -3,9 +3,9 @@
             [yaml-generator.controllers.yml :as controller.yml]))
 
 (defn generate-yml-handler
-  [message components]
-  (logs/log :info :generate-yml :received message)
-  (controller.yml/generate message components))
+  [{:keys [event]} components]
+  (logs/log :info :generate-yml :received event)
+  (controller.yml/generate event components))
 
 (def workers [{:handler generate-yml-handler
                :queue-name :mock.changed}])
