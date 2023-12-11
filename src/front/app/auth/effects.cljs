41f120c3988a7fd35eeda1a15729afcd978e8144
J0sueTM <teodoro.josue@protonmail.ch>
2023-12-11 14:05:38 +0000

    fix: remove duplicates

--- src/front/app/auth/effects.cljs
+++ src/front/app/auth/effects.cljs
@@ -2,17 +2,15 @@
   (:require
    [front.app.auth.supabase :as supabase]
    [promesa.core :as p]
-   [refx.alpha :refer [dispatch reg-fx]]))
-
-(defn- js->cljs-key [obj]
-  (js->clj obj :keywordize-keys true))
+   [refx.alpha :refer [dispatch reg-fx]]
+   [front.app.utils :as utils]))
 
 (defn send-email [{:keys [body on-success on-failure]}]
-  (js/console.log :sending-email)
+  (.log js/console :sending-email)
 
   (-> (supabase/signin-with-email supabase/client (:email body))
       (p/then (fn [resp]
-                (let [obj (js->cljs-key resp)]
+                (let [obj (utils/js->cljs-key resp)]
                   (if (:error obj)
                     (do
                       (js/console.error :resp-faiure obj)
@@ -20,12 +18,12 @@
                     (do
                       (js/console.log :resp-success obj)
                       (dispatch (conj on-success {:body (assoc
-                                                         (js->cljs-key resp)
+                                                         (utils/js->cljs-key resp)
                                                          :ok true)})))))))
 
       (p/catch (fn [resp]
                  (js/console.log :resp resp)
-                 (dispatch (conj on-failure {:body (js->cljs-key resp)}))))))
+                 (dispatch (conj on-failure {:body (utils/js->cljs-key resp)}))))))
 
 (defn auth-effect [fn-req]
   (fn [req]
