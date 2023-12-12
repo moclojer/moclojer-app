389fa9059f83df1d408f870cf606738786fdb0f9
J0sueTM <teodoro.josue@protonmail.ch>
2023-12-12 14:09:56 +0000

    fix: keyword parsing on error handling

--- src/back/api/interceptors/extract_user.clj
+++ src/back/api/interceptors/extract_user.clj
@@ -8,10 +8,16 @@
   (let [token (second (string/split auth #" "))]
     (jwt/unsign token secret {:alg :hs256})))
 
-(defn throw-unauthorized
-  ([] (throw-unauthorized "unauthorized"))
-  ([body]
-   (throw (ex-info "Unauthorized" {:cause body}))))
+(defn- get-message-when-keyword
+  "Some exceptions from buddy sign comes with a keyworded
+   cause, like {:cause :exp}, which crashes our server,
+   since Pedestal doesn't know how to transform a keyword
+   to a writeable response message."
+  [ex]
+  (let [cause (-> ex .getData :cause)]
+    (if (or (keyword? cause) (nil? cause))
+      (.getMessage ex)
+      cause)))
 
 (defn extract-user-interceptor []
   {:enter
@@ -20,17 +26,14 @@
        ctx
        (if-let [auth (get-in request [:headers "authorization"])]
          (let [secret (-> request :components :config :config :supabase-jwt-secret)
-               decoded (get-user-sub auth secret)]
-           (prn :auth auth :secret secret :decoded decoded)
-           (if decoded
-             (let [external-id (-> decoded :sub parse-uuid)
-                   database (-> request :components :database)
-                   user (->> external-id (db.customers/get-by-external-id database))
-                   session-data {:org (:customer/username user)
-                                 :user-id (:customer/uuid user)}]
-               (assoc-in ctx [:request :session-data] session-data))
-             (throw-unauthorized "token expired")))
-         (throw-unauthorized))))
+               decoded (get-user-sub auth secret)
+               database (-> request :components :database)
+               external-id (-> decoded :sub parse-uuid)
+               user (db.customers/get-by-external-id external-id database)
+               session-data {:org (:customer/username user)
+                             :user-id (:customer/uuid user)}]
+           (assoc-in ctx [:request :session-data] session-data))
+         (throw (ex-info "Unauthorized" {:cause "Token is missing"})))))
    ;; http://pedestal.io/pedestal/0.6/reference/error-handling.html
    :error
    (fn [ctx ex]
@@ -38,4 +41,4 @@
      (-> ctx
          (dissoc :error)
          (assoc :response {:status 401
-                           :body (-> ex .getData :cause)})))})
+                           :body (get-message-when-keyword ex)})))})
