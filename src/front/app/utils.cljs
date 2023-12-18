3b66d2c35ab1af9fb5104bb02ce6039a3cc0bcf4
J0sueTM <teodoro.josue@protonmail.ch>
2023-12-18 14:30:22 +0000

    chore: remove unused utils

--- src/front/app/utils.cljs
+++ src/front/app/utils.cljs
@@ -1,6 +1,4 @@
-(ns front.app.utils
-  (:require
-   [clojure.string :as str]))
+(ns front.app.utils)
 
 (defn replace-str [^String s]
   (.replace s "_" "-"))
@@ -16,14 +14,3 @@
 
 (defn js->cljs-key [obj]
   (js->clj obj :keywordize-keys true))
-
-(def ^:private callback-keys
-  ["access_token"
-   "expires_at"
-   "expires_in"
-   "token_type"
-   "type"])
-
-(defn check-in-callback?
-  ([] (check-in-callback? (.. js/window -location -href)))
-  ([href] (every? #(str/includes? href %) callback-keys)))
