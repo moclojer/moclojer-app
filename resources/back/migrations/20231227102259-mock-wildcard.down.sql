f345f7812c1f0e8ef982566f247908cb2c8702e3
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-01 14:08:17 +0000

    fix: create trigger to check wildcard uniqueness
    We were using a simple `CONSTRAINT UNIQUE(wildcard)`, but this would
    check for repeating constraints on the ENTIRE TABLE, which isn't what
    we want (to check just within the user's subdomain), and that's whats
    being done now.

--- resources/back/migrations/20231227102259-mock-wildcard.down.sql
+++ resources/back/migrations/20231227102259-mock-wildcard.down.sql
@@ -1,1 +1,7 @@
-ALTER TABLE mock DROP CONSTRAINT uq_mock_wildcard;
+ALTER TABLE Mock DROP CONSTRAINT IF EXISTS uq_wildcard_per_user_subdomain;
+
+DROP TRIGGER IF EXISTS enforce_uq_wildcard ON mock;
+
+DROP FUNCTION IF EXISTS check_unique_wildcard();
+
+DROP INDEX IF EXISTS uq_wildcard_per_user_subdomain;
--- resources/back/migrations/20231227102259-mock-wildcard.up.sql
+++ resources/back/migrations/20231227102259-mock-wildcard.up.sql
@@ -1,1 +1,23 @@
-ALTER TABLE mock ADD CONSTRAINT uq_mock_wildcard UNIQUE(wildcard);
+CREATE OR REPLACE FUNCTION check_uq_wildcard()
+RETURNS TRIGGER AS $$
+BEGIN
+  IF EXISTS (
+     SELECT 1
+     FROM mock
+     WHERE user_id = NEW.user_id
+       AND subdomain = NEW.subdomain
+       AND wildcard = NEW.wildcard
+  ) THEN
+      RAISE EXCEPTION 'Duplicate wildcard for this user subdomain';
+  END IF;
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER enforce_uq_wildcard
+BEFORE INSERT ON mock
+FOR EACH ROW EXECUTE FUNCTION check_uq_wildcard();
+
+CREATE UNIQUE INDEX uq_wildcard_per_user_subdomain
+ON mock (user_id, subdomain, wildcard);
