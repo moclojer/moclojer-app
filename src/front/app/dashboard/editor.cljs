9119cd96b61616fb89e6b7191c008537b1689215
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-15 08:44:36 +0000

    style: mv linting to unique file

--- src/front/app/dashboard/editor.cljs
+++ src/front/app/dashboard/editor.cljs
@@ -5,85 +5,15 @@
    ["@codemirror/lint" :as clint]
    ["@uiw/codemirror-theme-github" :as theme]
    ["@uiw/react-codemirror" :as c]
-   ["ajv" :as ajv]
-   ["js-yaml" :as js-yaml]
-   [clojure.string :refer [join]]
    [front.app.components.svg :as svg]
    [front.app.dashboard.base :as base]
+   [front.app.dashboard.linter :as linter]
    [front.app.lib :refer [defnc]]
    [helix.core :refer [$]]
    [helix.dom :as d]
    [helix.hooks :as hooks]
-   [lambdaisland.fetch :as fetch]
-   [promesa.core :as p]
    [refx.alpha :as refx]
    [reitit.frontend.easy :as rfe]))
-
-;; TODO: maybe block mock saving if failed to retrieve schema?
-;; if we can't retrieve a static resource, then there's a deeper
-;; problem.
-
-;; NOTE: AJV works in a closure-like model. A function that returns
-;; other functions which know about their innerstate.
-;; This is why this is a function (that also returns a function XD).
-;; Incase we fail to retrieve the moclojer schema, we return a function
-;; that returns `false` everytime
-
-(defn gen-ajv-validate-fn [raw-schema]
-  (or (try
-        (let [ajv-inst (ajv. #js {:allErrors true
-                                  :jsonPointers true})]
-          (->> raw-schema
-               (.load js-yaml)
-               (.compile ajv-inst)))
-        (catch :default e
-          (.error js/console "failed to load moclojer schema: " e)
-          nil))
-      #(constantly false)))
-
-(def validate-moclojer-yaml (atom #(constantly false)))
-
-(defn get-validate-moclojer-yaml-fn []
-  (-> (fetch/get "/assets/yaml/moclojer-schema.yaml")
-      (p/then
-       #(reset! validate-moclojer-yaml (gen-ajv-validate-fn (:body %))))
-      (p/catch
-       #(do
-          (.error js/console "failed to retrieve moclojer schema: " %)
-          ;; nested `#` are not allowed
-          (reset! validate-moclojer-yaml (fn [] (constantly true)))))))
-
-(defn handle-lint-ex [ex]
-  (let [mark (.-mark ex)
-        from (if mark (dec (.-position mark)) 0)]
-    (clj->js [{:from from :to from
-               :message (.-reason ex)
-               :severity "error"}])))
-
-;; TODO: for now, we'll be rendering one error at a time.
-(defn handle-validation-errs [errs]
-  (let [err (first ^js errs)]
-    (clj->js [{:from 0 :to 0
-               :message (str (.-keyword err) " " (.-message err))
-               :severity "error"}])))
-
-(def yaml-linter
-  (clint/linter
-   (fn [view]
-     (let [doc (.. view -state -doc)
-           content (join \newline (.-text doc))
-           ;; so we don't raise an error when there's no text
-           validate (if (not-empty content)
-                      @validate-moclojer-yaml
-                      nil?)
-           diagnostics (try
-                         (if-not (-> (.load js-yaml content)
-                                     validate)
-                           (handle-validation-errs (.-errors validate))
-                           #js [])
-                         (catch :default ex (handle-lint-ex ex)))]
-       (refx/dispatch-sync [:app.dashboard/set-mock-validation (empty? diagnostics)])
-       diagnostics))))
 
 (defnc editor [props]
   (let [{:keys [data]} props
@@ -94,17 +24,13 @@
                   (refx/dispatch-sync
                    [:app.dashboard/edit-mock {:mock-id id
                                               :content content}])))]
-    (hooks/use-effect
-     :once
-     (get-validate-moclojer-yaml-fn))
-
     ($ c/default
        {:autoFocus true
         :value (or content "")
         :height "400px"
         :theme theme/githubLight
         :extensions #js [(.define language/StreamLanguage yaml/yaml)
-                         (clint/lintGutter) yaml-linter]
+                         (clint/lintGutter) linter/yaml-linter]
         :onChange (partial ref-fn)
         :mode "yaml"})))
 
