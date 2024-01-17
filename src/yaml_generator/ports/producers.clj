362c48b894d274885df0b0e5bc7c351a6835e19f
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-17 18:32:26 +0000

    refactor: send domain in event instead of parsing from content

--- src/yaml_generator/ports/producers.clj
+++ src/yaml_generator/ports/producers.clj
@@ -4,26 +4,26 @@
    [components.redis-publisher :as redis-publisher]))
 
 (defn mock-unified!
-  [path publisher]
+  [path domain publisher]
   (try
-    (let [_ (redis-publisher/publish! publisher :mock.unified {:event {:path path}})]
-      path)
+    (redis-publisher/publish! publisher :mock.unified
+                              {:event {:path path
+                                       :domain domain}})
     (catch Exception e
-      (logs/log :error e)
       (logs/log :error :publish-mock-error e))))
 
 (defn restart-mock!
   [publisher]
   (try
-    (let [_ (redis-publisher/publish! publisher :restart.mocks
-                                      {:event {:path "moclojer.yml"}})])
+    (redis-publisher/publish! publisher :restart.mocks
+                              {:event {:path "moclojer.yml"}})
     (catch Exception e
       (logs/log :error :publish-mock-restart-error e))))
 
 (defn create-domain!
   [domain publisher]
   (try
-    (let [_ (redis-publisher/publish! publisher :domain.create
-                                      {:event {:domain domain}})])
+    (redis-publisher/publish! publisher :domain.create
+                              {:event {:domain domain}})
     (catch Exception e
       (logs/log :error :publish-domain-error e))))
