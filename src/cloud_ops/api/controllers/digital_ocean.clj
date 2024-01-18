9ff0c1570844734cc5051e5e0722c301555ae60d
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-18 12:32:22 +0000

    refactor: create abstraction over DO API requests

--- src/cloud_ops/api/controllers/digital_ocean.clj
+++ src/cloud_ops/api/controllers/digital_ocean.clj
@@ -1,18 +1,12 @@
 (ns cloud-ops.api.controllers.digital-ocean
   (:require
    [cloud-ops.api.logic.digital-ocean :as logic.do]
-   [cloud-ops.api.ports.http-out :as http-out]
-   [components.logs :as logs]))
-
-(defn get-current-spec [config]
-  (let [{:keys [base-url app-id token]} (get-in config
-                                                [:cloud-providers
-                                                 :digital-ocean])]
-    (logs/log :info :config base-url app-id token)
-    (http-out/get-current-do-spec base-url app-id token)))
+   [cloud-ops.api.ports.http-out :as http-out]))
 
 (defn create-domain! [domain {:keys [config]}]
-  (some->
-   (:config config)
-   get-current-spec
-   (logic.do/add-domain-to-spec domain)))
+  (let [provider (get-in config [:config :cloud-providers :digital-ocean])
+        {:keys [base-url app-id token]} provider
+        req-params (http-out/mount-req base-url app-id token)]
+    (some->
+     (http-out/get-current-do-spec req-params)
+     (logic.do/add-domain-to-spec domain))))
