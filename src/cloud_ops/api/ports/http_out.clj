25b7a312d54faf445cb7dfe6e4a784b43788b5fa
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-18 12:32:22 +0000

    refactor: create abstraction over DO API requests

--- src/cloud_ops/api/ports/http_out.clj
+++ src/cloud_ops/api/ports/http_out.clj
@@ -4,20 +4,22 @@
    [components.logs :as logs]
    [muuntaja.core :as m]))
 
+(defn mount-req
+  "Creates a list of parameters that can be later used
+   on http requests to digitalocean API with `apply`."
+  [base-url app-id token]
+  (let [url (str base-url "/apps/" app-id)
+        headers {:Content-Type "application/json"
+                 :Authorization (str "Bearer " token)}]
+    [url {:headers headers}]))
+
 (defn get-current-do-spec
   "Retrieves the current App Specification from the `moclojer-cloud` app in DO.
    This spec contains everything (literally everything) there is stored about
-   the app. We actually don't need all of that, so we clean it afterwards.
-
-   `base-url`, `app-id` and `auth-token` are constant values defined in
-   `resources/back/config.edn`."
-  [base-url app-id auth-token]
+   the app. We actually don't need all of that, so we clean it afterwards."
+  [req-params]
   (try
-    (let [url (str base-url "/apps/" app-id)
-          {:keys [body]} (c/get
-                          url
-                          {:headers {:Content-Type "application/json"
-                                     :Authorization (str "Bearer " auth-token)}})
+    (let [{:keys [body]} (apply c/get req-params)
           decoded (m/decode "application/json" body)
           spec (get-in decoded [:app :spec])]
       (logs/log :info :domain-create :spec-ok)
@@ -25,3 +27,6 @@
     (catch Exception e
       (logs/log :error :domain-create-error e))))
 
+#_(defn update-do-spec!
+    "Updates Digital Ocean `spec`."
+    [req-params])
