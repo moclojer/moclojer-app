218e35eda8129ab3aeb43da569d2d1870200ccac
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-22 15:17:45 +0000

    feat: implement job retry logic

---
+++ src/cloud_ops/api/ports/producers.clj
@@ -0,0 +1,13 @@
+(ns cloud-ops.api.ports.producers
+  (:require
+   [components.logs :as logs]
+   [components.redis-publisher :as redis-publisher]))
+
+(defn verify-domain!
+  [domain attempt publisher]
+  (try
+    (redis-publisher/publish! publisher :domain.verify
+                              {:event {:domain domain
+                                       :attempt attempt}})
+    (catch Exception e
+      (logs/log :error :verify-domain e))))
--- src/cloud_ops/api/ports/workers.clj
+++ src/cloud_ops/api/ports/workers.clj
@@ -1,14 +1,24 @@
 (ns cloud-ops.api.ports.workers
   (:require
-   [cloud-ops.api.controllers.provider :as ctrls.provider]
+   [cloud-ops.api.controllers.cloud :as ctrls.cloud]
+   [cloud-ops.api.ports.producers :as producers]
    [components.logs :as logs]))
 
 (defn create-domain-handler
-  [{:keys [event]} components]
+  [{:keys [event]} {:keys [publisher] :as components}]
   (if-let [domain (:domain event)]
-    (some-> (ctrls.provider/get-current-data domain components)
-            (ctrls.provider/create-domain! domain components))
+    (some-> (ctrls.cloud/get-current-data domain components)
+            (ctrls.cloud/create-domain! domain components)
+            (producers/verify-domain! 1 publisher))
     (logs/log :error :create-domain :empty-domain)))
 
+(defn verify-domain-handler
+  [{:keys [event]} components]
+  (if (:domain event)
+    (ctrls.cloud/verify-domain event components)
+    (logs/log :error :verify-domain :empty-domain)))
+
 (def workers [{:handler create-domain-handler
-               :queue-name :domain.create}])
+               :queue-name :domain.create}
+              {:handler verify-domain-handler
+               :queue-name :domain.verify}])
