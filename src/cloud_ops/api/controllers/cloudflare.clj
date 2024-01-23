d0d2e64239c5a922fd9b693b85ea4636613bf62d
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-23 21:09:15 +0000

    fix: rm specific cloudflare get-in

--- src/cloud_ops/api/controllers/cloudflare.clj
+++ src/cloud_ops/api/controllers/cloudflare.clj
@@ -3,16 +3,14 @@
    [cloud-ops.api.ports.http-out :as http-out]))
 
 (defn get-current-records [{:keys [config http]}]
-  (let [{:keys [cloudflare
-                proxy]} (get-in config [:config :cloud-providers])
+  (let [{:keys [cloudflare proxy]} (get-in config [:config :cloud-providers])
         {:keys [base-url zone-id token]} cloudflare
         url (str base-url "/zones/" zone-id "/dns_records")
         req-params (http-out/mount-basic-req url token proxy)]
     (http-out/get-current-cf-records http req-params)))
 
 (defn create-domain! [domain {:keys [config http]}]
-  (let [{:keys [cloudflare
-                proxy]} (get-in config [:config :cloud-providers :cloudflare])
+  (let [{:keys [cloudflare proxy]} (get-in config [:config :cloud-providers])
         {:keys [base-url zone-id token record-content]} cloudflare
         url (str base-url "/zones/" zone-id "/dns_records")
         req-params (http-out/mount-basic-req url token proxy)]
