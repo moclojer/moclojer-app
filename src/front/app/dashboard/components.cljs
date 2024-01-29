4bb6e6f62aad2a14247c806121da08bc0f740d93
J0sueTM <teodoro.josue@protonmail.ch>
2024-01-29 10:06:51 +0000

    feat: update logo to new version

--- src/front/app/dashboard/components.cljs
+++ src/front/app/dashboard/components.cljs
@@ -33,7 +33,6 @@
             (d/button {:id "toggleSidebarMobile"
                        :class-name "p-2 mr-2 text-gray-600 rounded cursor-pointer lg:hidden hover:text-gray-900 hover:bg-gray-100 focus:bg-gray-100 dark:focus:bg-gray-700 focus:ring-2 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                        :aria-expanded "true"
-
                        :aria-controls  "sidebar"}
                       (d/svg {:id "toggleSidebarMobileHamburger"
                               :class-name "w-6 h-6"
@@ -49,10 +48,10 @@
                              (d/path {:fill-rule "evenodd"
                                       :d "M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                       :clip-rule "evenodd"})))
-            (d/a {:href "#" :class-name "flex mr-14"}
-                 (d/img {:src "/images/logo.png"
-                         :class-name "mr-3 h-8"})
-                 (d/span {:class-name "self-center text-2xl font-semibold whitespace-nowrap dark:text-white"})))
+            (d/button {:class-name "flex mr-14"
+                       :on-click #(rfe/push-state :app.core/dashboard)}
+                      (d/img {:src "/images/logo.svg"
+                              :class-name "mr-3 h-9"})))
      (d/div {:class-name "flex items-center lg:gap-3"}
             (d/div {:class-name "px-3 py-2 bg-pink-600 rounded-lg justify-end items-center gap-2 flex btn-add"}
                    (d/svg {:width "16"
