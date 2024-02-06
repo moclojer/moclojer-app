60f2fa4e9c171b4b184d443fcf956021f3883d83
J0sueTM <teodoro.josue@protonmail.ch>
2024-02-06 16:07:14 +0000

    feat: show helper on mock list whenever there are no mocks

--- src/front/app/dashboard/components.cljs
+++ src/front/app/dashboard/components.cljs
@@ -94,6 +94,21 @@
                            :role "menuitem"}
                           "Logout")))))))
 
+(defnc new-mock-btn []
+  (d/button {:class-name "px-3 py-2 bg-pink-600 rounded-lg justify-end items-center gap-2 flex btn-add"
+             :on-click #(refx/dispatch [:app.dashboard/toggle-mock-modal])}
+            (d/svg {:width "16"
+                    :height "17"
+                    :viewBox "0 0 16 17"
+                    :fill "none"}
+                   (d/path {:fill-rule "evenodd"
+                            :clip-rule "evenodd"
+                            :d "M8 4.5C8.21217 4.5 8.41566 4.58429 8.56569 4.73431C8.71571 4.88434 8.8 5.08783 8.8 5.3V7.7H11.2C11.4122 7.7 11.6157 7.78429 11.7657 7.93431C11.9157 8.08434 12 8.28783 12 8.5C12 8.71217 11.9157 8.91566 11.7657 9.06569C11.6157 9.21571 11.4122 9.3 11.2 9.3H8.8V11.7C8.8 11.9122 8.71571 12.1157 8.56569 12.2657C8.41566 12.4157 8.21217 12.5 8 12.5C7.78783 12.5 7.58434 12.4157 7.43431 12.2657C7.28429 12.1157 7.2 11.9122 7.2 11.7V9.3H4.8C4.58783 9.3 4.38434 9.21571 4.23431 9.06569C4.08429 8.91566 4 8.71217 4 8.5C4 8.28783 4.08429 8.08434 4.23431 7.93431C4.38434 7.78429 4.58783 7.7 4.8 7.7H7.2V5.3C7.2 5.08783 7.28429 4.88434 7.43431 4.73431C7.58434 4.58429 7.78783 4.5 8 4.5V4.5Z"
+                            :fill "white"}))
+            (d/p {:class-name "text-white text-xs font-bold leading-[18px]"}
+                 " new mock")
+            ($ svg/box)))
+
 (defnc nav-bar [{:keys [aside-open? toggle-aside! user-data]}]
   (d/nav
    {:class-name "fixed z-30 w-full bg-white border-b border-gray-200 dark:bg-gray-800 dark:border-gray-700"}
@@ -117,19 +132,7 @@
                       (d/img {:src "/images/logo.svg"
                               :class-name "mr-3 h-9"})))
      (d/div {:class-name "flex items-center lg:gap-3"}
-            (d/button {:class-name "px-3 py-2 bg-pink-600 rounded-lg justify-end items-center gap-2 flex btn-add"
-                       :on-click (fn [_] (refx/dispatch [:app.dashboard/toggle-mock-modal]))}
-                      (d/svg {:width "16"
-                              :height "17"
-                              :viewBox "0 0 16 17"
-                              :fill "none"}
-                             (d/path {:fill-rule "evenodd"
-                                      :clip-rule "evenodd"
-                                      :d "M8 4.5C8.21217 4.5 8.41566 4.58429 8.56569 4.73431C8.71571 4.88434 8.8 5.08783 8.8 5.3V7.7H11.2C11.4122 7.7 11.6157 7.78429 11.7657 7.93431C11.9157 8.08434 12 8.28783 12 8.5C12 8.71217 11.9157 8.91566 11.7657 9.06569C11.6157 9.21571 11.4122 9.3 11.2 9.3H8.8V11.7C8.8 11.9122 8.71571 12.1157 8.56569 12.2657C8.41566 12.4157 8.21217 12.5 8 12.5C7.78783 12.5 7.58434 12.4157 7.43431 12.2657C7.28429 12.1157 7.2 11.9122 7.2 11.7V9.3H4.8C4.58783 9.3 4.38434 9.21571 4.23431 9.06569C4.08429 8.91566 4 8.71217 4 8.5C4 8.28783 4.08429 8.08434 4.23431 7.93431C4.38434 7.78429 4.58783 7.7 4.8 7.7H7.2V5.3C7.2 5.08783 7.28429 4.88434 7.43431 4.73431C7.58434 4.58429 7.78783 4.5 8 4.5V4.5Z"
-                                      :fill "white"}))
-                      (d/p {:class-name "text-white text-xs font-bold leading-[18px]"}
-                           " new mock")
-                      ($ svg/box))
+            ($ new-mock-btn)
             ($ user-profile {:user-data user-data}))))))
 
 (defnc aside [{:keys [aside-open? toggle-aside!]}]
@@ -177,10 +180,7 @@
                                                (d/li
                                                 (d/div {:class "flex flex-row"}
                                                        (d/button
-                                                        {:on-click (fn [_e]
-                                                                     (if (empty? mocks-raw)
-                                                                       (refx/dispatch-sync [:app.dashboard/toggle-mock-modal])
-                                                                       (rfe/push-state :app.core/mocks)))
+                                                        {:on-click #(rfe/push-state :app.core/mocks)
                                                          :class-name "flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
                                                          :aria-controls "dropdown-mocks"
                                                          :aria-expanded aside-open?
@@ -195,19 +195,23 @@
                                                 (d/ul {:id "dropdown-mocks"
                                                        :class-name (str "py-2 space-y-2"
                                                                         (when (or (not menu-open?) (not aside-open?)) " hidden"))}
-                                                      (for [{:keys [mock-type wildcard id]
-                                                             :or {mock-type :personal}} mocks-raw]
-                                                        (d/li
-                                                         {:key wildcard}
-                                                         (d/button {:on-click #(rfe/push-state :app.core/mocks-view {:mock-id id})
-                                                                    :class-name (str "pl-11 w-full flex items-center overflow-hidden ellipsis p-2 "
-                                                                                     "text-base font-normal text-gray-900 rounded-lg transition "
-                                                                                     "duration-75 group hover:bg-gray-100 dark:text-gray-200 "
-                                                                                     "dark:hover:bg-gray-700")}
-                                                                   (if (= mock-type :personal)
-                                                                     ($ svg/personal-mock)
-                                                                     ($ svg/org-mock))
-                                                                   (d/span {:class-name "flex-1 ml-3 text-left truncate"} wildcard)))))))
+                                                      (if (empty? mocks-raw)
+                                                        (d/div {:class "w-full pl-11 space-y-2"}
+                                                               (d/p {:class "text-gray-700 text-sm"} "You currently don't have any mocks. Create one!")
+                                                               ($ new-mock-btn))
+                                                        (for [{:keys [mock-type wildcard id]
+                                                               :or {mock-type :personal}} mocks-raw]
+                                                          (d/li
+                                                           {:key wildcard}
+                                                           (d/button {:on-click #(rfe/push-state :app.core/mocks-view {:mock-id id})
+                                                                      :class-name (str "pl-11 w-full flex items-center overflow-hidden ellipsis p-2 "
+                                                                                       "text-base font-normal text-gray-900 rounded-lg transition "
+                                                                                       "duration-75 group hover:bg-gray-100 dark:text-gray-200 "
+                                                                                       "dark:hover:bg-gray-700")}
+                                                                     (if (= mock-type :personal)
+                                                                       ($ svg/personal-mock)
+                                                                       ($ svg/org-mock))
+                                                                     (d/span {:class-name "flex-1 ml-3 text-left truncate"} wildcard))))))))
                                          (d/div {:class-name "pt-2 space-y-2"}
                                                 (d/a {:href ""
                                                       :target "_blank"
