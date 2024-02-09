c66ee9a793083886d9951ff284a7f627ca3d9be8
J0sueTM <teodoro.josue@protonmail.ch>
2024-02-09 13:45:40 +0000

    feat: change team message date to be more concise

--- src/front/app/dashboard/views.cljs
+++ src/front/app/dashboard/views.cljs
@@ -1,7 +1,7 @@
 (ns front.app.dashboard.views
   (:require
-   [front.app.lib :refer [defnc]]
    [front.app.dashboard.base :as base]
+   [front.app.lib :refer [defnc]]
    [helix.core :refer [$]]
    [helix.dom :as d]))
 
@@ -11,7 +11,7 @@
       (d/div {:class-name "p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700"}
              (d/div {:class-name "flex items-center divide-x divide-gray-100 dark:divide-gray-700"}
                     (d/div {:class-name "pl-3 text-sm font-medium text-gray-500"}
-                           "Today, 11:01 AM")))
+                           "Today")))
       (d/div {:class-name "p-5"}
              (d/div {:class-name "flex items-center mb-4"}
                     (d/div {:class-name "flex-shrink-0"}
