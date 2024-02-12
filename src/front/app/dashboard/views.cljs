eb57c7e521236a51da0c3b20cfd2b3dfbd711a46
J0sueTM <teodoro.josue@protonmail.ch>
2024-02-12 15:37:34 +0000

    feat: format current date instead of literal "today"

--- src/front/app/dashboard/views.cljs
+++ src/front/app/dashboard/views.cljs
@@ -1,9 +1,18 @@
 (ns front.app.dashboard.views
   (:require
+   [clojure.string :as str]
    [front.app.dashboard.base :as base]
    [front.app.lib :refer [defnc]]
    [helix.core :refer [$]]
    [helix.dom :as d]))
+
+(defn gen-today-date []
+  (let [date (js/Date.)
+        pad-comp-fn #(-> (.toString %)
+                         (.padStart 2 "0"))]
+    (->> [(.getDate date) (inc (.getMonth date)) (.getFullYear date)]
+         (map pad-comp-fn)
+         (str/join "/"))))
 
 (defnc welcome []
   ($ base/index
@@ -11,7 +20,7 @@
       (d/div {:class-name "p-4 bg-white block sm:flex items-center justify-between border-b border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700"}
              (d/div {:class-name "flex items-center divide-x divide-gray-100 dark:divide-gray-700"}
                     (d/div {:class-name "pl-3 text-sm font-medium text-gray-500"}
-                           "Today")))
+                           (gen-today-date))))
       (d/div {:class-name "p-5"}
              (d/div {:class-name "flex items-center mb-4"}
                     (d/div {:class-name "flex-shrink-0"}
