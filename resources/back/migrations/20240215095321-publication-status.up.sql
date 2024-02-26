9835d2ae29e1d0dbbe13cea9a5012a0afee16e56
J0sueTM <teodoro.josue@protonmail.ch>
2024-02-26 17:27:32 +0000

    fix: cover existing mocks on `not null` constraint migration

--- resources/back/migrations/20240215095321-publication-status.up.sql
+++ resources/back/migrations/20240215095321-publication-status.up.sql
@@ -1,3 +1,7 @@
 CREATE TYPE publication_status AS ENUM('offline', 'publishing', 'published');
 --;;
-ALTER TABLE mock ADD COLUMN publication publication_status NOT NULL;
+ALTER TABLE mock ADD COLUMN publication publication_status DEFAULT 'offline';
+--;;
+UPDATE mock SET publication = 'published' WHERE publication IS NULL;
+--;;
+ALTER TABLE mock ALTER COLUMN publication SET NOT NULL;
