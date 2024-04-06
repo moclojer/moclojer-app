22c78074e4f3ce3bfed6fbf06d8cdf0d7b143b93
J0sueTM <teodoro.josue@protonmail.ch>
2024-04-06 02:37:00 +0000

    fix: separate sentry fns from component

--- src/components/sentry.clj
+++ src/components/sentry.clj
@@ -7,9 +7,18 @@
   (:import
    [io.sentry CustomSamplingContext]))
 
-(defprotocol SentryLogger
-  (set-default-exception-handler [this])
-  (send-event [this event]))
+(defn send-event [event]
+  (try
+    (sentry/send-event event)
+    (catch Exception e
+      (println "failed to submit event to sentry" (ex-data e)))))
+
+(defn set-default-exception-handler []
+  (Thread/setDefaultUncaughtExceptionHandler
+   (reify Thread$UncaughtExceptionHandler
+     (uncaughtException [_ _ exception]
+       (logs/log :warn :uncaught-exception)
+       (send-event {:throwable exception})))))
 
 (defrecord Sentry [config]
   component/Lifecycle
@@ -17,38 +26,26 @@
     (let [sentry-cfg (get-in config [:config :sentry])]
       (when (:dns sentry-cfg)
         (println "starting sentry" :env (:env sentry-cfg))
-        (sentry/init! (:dns sentry-cfg) sentry-cfg)))
+        (sentry/init! (:dns sentry-cfg) sentry-cfg)
+        (set-default-exception-handler)))
     this)
-  (stop [_])
-  SentryLogger
-  (set-default-exception-handler [this]
-    (Thread/setDefaultUncaughtExceptionHandler
-     (reify Thread$UncaughtExceptionHandler
-       (uncaughtException [_ _ exception]
-         (logs/log :warn :uncaught-exception)
-         (send-event this {:throwable exception})))))
-  (send-event [_ event]
-    (try
-      (sentry/send-event event)
-      (catch Exception e
-        (println "failed to submit event to sentry" (ex-data e))))))
+  (stop [_]))
 
 (defn new-sentry [config]
   (->Sentry config))
 
 (comment
-  (def sss
-    (-> {:config {:sentry {:dns "foobar"
-                           :traces-sample-rate 1.0
-                           :env "prod"}}}
-        new-sentry
-        component/start))
+  (-> {:config {:sentry {:dns "foobar"
+                         :traces-sample-rate 1.0
+                         :env "prod"}}}
+      new-sentry
+      component/start)
 
-  (send-event sss {:message "Oh no!"
-                   :throwable (RuntimeException. "Something has happened")})
+  (send-event  {:message "Oh no!"
+                :throwable (RuntimeException. "Something has happened")})
 
   (sentry-tr/with-start-child-span "task" "logging in"
-    (send-event sss {:throwable (ex-info "failed to log in" {})}))
+    (send-event {:throwable (ex-info "failed to log in" {})}))
 
   (let [tr (sentry-tr/start-transaction "update-user" "Updates a user in db"
                                         (CustomSamplingContext.)
@@ -56,8 +53,7 @@
     (sentry-tr/with-start-child-span "get-user" "getting user"
       (Thread/sleep 2000)
       (sentry-tr/with-start-child-span "update-user" "updating user"
-        (send-event sss
-                    {:user {:id (str (random-uuid))
+        (send-event {:user {:id (str (random-uuid))
                             :email "teodoro.josue@pm.me"}
                      :breadcrumbs [{:type "http"
                                     :category "xhr"
