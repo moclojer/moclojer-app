a2b01abcfec89e684169283f813e53a3aa1d9123
j0suetm <teodoro.josue@protonmail.ch>
2024-04-19 10:42:01 -0300

    feat: use json logging format on prod

--- src/components/logs.clj
+++ src/components/logs.clj
@@ -1,13 +1,16 @@
 (ns components.logs
   (:require
    [taoensso.timbre :as timbre]
-   [taoensso.timbre.appenders.core :as core-appenders]))
+   [taoensso.timbre.appenders.core :as core-appenders]
+   [timbre-json-appender.core :as tas]))
 
-(defn setup [level stream]
+(defn setup [level stream env]
   (timbre/set-level! level)
   (timbre/merge-config!
    {:appenders
-    {:println (core-appenders/println-appender {:stream stream})}}))
+    (if (= env :dev)
+      {:println (core-appenders/println-appender {:stream stream})}
+      {:json (tas/json-appender)})}))
 
 (defmacro log [level & args]
   `(timbre/log ~level ~@args))
