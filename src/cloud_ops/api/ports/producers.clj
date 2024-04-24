11311d37abfbdc37c95266f24f46643b98dc5ed1
j0suetm <teodoro.josue@protonmail.ch>
2024-04-23 23:10:38 -0300

    refactor: stop logging redis exceptions

--- src/cloud_ops/api/ports/producers.clj
+++ src/cloud_ops/api/ports/producers.clj
@@ -1,23 +1,17 @@
 (ns cloud-ops.api.ports.producers
-  (:require
-   [components.logs :as logs]
-   [components.redis-publisher :as redis-publisher]))
+  (:require [components.logs :as logs]
+            [components.redis-publisher :as redis-publisher]))
 
 (defn verify-domain!
   [domain attempt publisher]
-  (try
-    (redis-publisher/publish! publisher "domain.verify"
-                              {:event {:domain domain
-                                       :attempt attempt}})
-    (catch Exception e
-      (logs/log :error :verify-domain e))))
+  (redis-publisher/publish! publisher "domain.verify"
+                            {:event {:domain domain
+                                     :attempt attempt}}))
 
 (defn set-publication-status!
   [domain new-status publisher]
-  (try
-    (redis-publisher/publish! publisher "mock.publication"
-                              {:event {:domain domain
-                                       :new-status new-status}})
-    domain
-    (catch Exception e
-      (logs/log :error :set-publication-status e))))
+  (redis-publisher/publish! publisher "mock.publication"
+                            {:event {:domain domain
+                                     :new-status new-status}})
+
+  domain)
