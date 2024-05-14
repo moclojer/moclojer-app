2cf4ccb3ad321a250a73603f8c4fd8b52f73fe28
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-05-14 09:03:50 -0300

    refactor: move anon verify!-fn to priv helper

--- src/cloud_ops/api/controllers/cloud.clj
+++ src/cloud_ops/api/controllers/cloud.clj
@@ -14,6 +14,14 @@
   {:cf-records (controllers.cf/get-current-records components)
    :do-spec (controllers.do/get-current-spec components)})
 
+(defn- produce-verify-domain!
+  "Helper function for producing the `verify-health :domain` event"
+  [domain retrying? skip-data? publisher]
+  (producers/verify-health! :domain {:domain domain
+                                     :retrying? retrying?
+                                     :skip-data? skip-data?}
+                            publisher))
+
 (defn create-domain!
   "Calls domain creation controllers for both CloudFlare and DigitalOcean,
    making some safety checks beforehand."
@@ -27,19 +35,16 @@
                                       domain)
         do-domains (:domains do-spec)
         ;; a nil data means it was removed for already existing
-        both-exist? (and (nil? cf-records) (nil? do-domains))
-        ;; opposing `retrying?` so we just do it once
-        verify!-fn #(producers/verify-health! :domain {:domain domain
-                                                       :retrying? %1
-                                                       :skip-data? %2}
-                                              publisher)]
+        both-exist? (and (nil? cf-records) (nil? do-domains))]
+
+    ;; opposing `retrying?` so we just do it once
     (if both-exist?
-      (verify!-fn false true)
+      (produce-verify-domain! domain (not retrying?) retrying? true)
       (do
         (producers/set-publication-status! domain "publishing" publisher)
         (controllers.cf/create-domain! cf-records domain components)
         (controllers.do/create-domain! do-spec domain components)
-        (verify!-fn (not retrying?) false)))))
+        (produce-verify-domain! domain (not retrying?) retrying? false)))))
 
 ;; Not sure if this is the best option, but works fine for now,
 ;; since it isn't expected to have too many verifications ongoing
