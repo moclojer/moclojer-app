fc41b45d26d7653eb135c54134caa8d18293fa58
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-06-19 08:42:43 -0300

    feat: slugify and clean username instead of just lowercasing

--- src/back/api/logic/customers.clj
+++ src/back/api/logic/customers.clj
@@ -1,5 +1,6 @@
 (ns back.api.logic.customers
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [slugify.core :refer [slugify]]))
 
 (defn create
   [{:keys [email id]}]
@@ -7,6 +8,9 @@
    :customer/email email
    :customer/external-uuid (parse-uuid id)})
 
+;; since sub-domains are the ones we worry about separating
+;; with `-`, the username shouldn't have `-`s, or it will clash
+;; with the sub-domains. This is why we remove them.
 (defn add-username [user username]
   [user username]
-  (assoc user :customer/username (str/lower-case username)))
+  (assoc user :customer/username (str/replace (slugify username) #"-" "")))
