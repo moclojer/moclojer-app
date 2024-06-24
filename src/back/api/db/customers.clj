2d1b1ad0c314df066dc355a047be311d8b27d75e
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-06-24 11:15:29 -0300

    feat: conform to logging capabilities of db component

--- src/back/api/db/customers.clj
+++ src/back/api/db/customers.clj
@@ -1,56 +1,50 @@
 (ns back.api.db.customers
-  (:require [com.moclojer.components.database :as database]
+  (:require [back.api.db.utils :as db.utils]
             [honey.sql :as sql]
             [honey.sql.helpers :as sql.helpers]))
 
 (defn insert!
-  [transaction db]
-  (->> (-> (sql.helpers/insert-into :customer)
-           (sql.helpers/values [transaction])
-           (sql.helpers/returning :*)
-           sql/format)
-       (database/execute db)
-       first))
+  [transaction db ctx]
+  (-> (sql.helpers/insert-into :customer)
+      (sql.helpers/values [transaction])
+      (sql.helpers/returning :*)
+      sql/format
+      ((db.utils/build-execute-with-ctx db ctx))))
 
 (defn update!
-  [{:customer/keys [uuid] :as user} db]
-  (->> (-> (sql.helpers/update :customer)
-           (sql.helpers/set user)
-           (sql.helpers/where [:= :uuid uuid])
-           (sql/format))
-       (database/execute db)
-       first))
+  [{:customer/keys [uuid] :as user} db ctx]
+  (-> (sql.helpers/update :customer)
+      (sql.helpers/set user)
+      (sql.helpers/where [:= :uuid uuid])
+      sql/format
+      ((db.utils/build-execute-with-ctx db ctx))))
 
-(defn get-customers [db]
-  (database/execute
-   db
-   (-> (sql.helpers/select :*)
-       (sql.helpers/from :customer)
-       sql/format)))
+(defn get-customers [db ctx]
+  (-> (sql.helpers/select :*)
+      (sql.helpers/from :customer)
+      sql/format
+      ((db.utils/build-execute-with-ctx db ctx))))
 
-(defn get-by-external-id [id db]
-  (-> (database/execute
-       db
-       (-> (sql.helpers/select :*)
-           (sql.helpers/from :customer)
-           (sql.helpers/where [:= :external_uuid id])
-           sql/format))
+(defn get-by-external-id [id db ctx]
+  (-> (sql.helpers/select :*)
+      (sql.helpers/from :customer)
+      (sql.helpers/where [:= :external_uuid id])
+      sql/format
+      ((db.utils/build-execute-with-ctx db ctx))
       first))
 
-(defn get-by-id [id db]
-  (-> (database/execute
-       db
-       (-> (sql.helpers/select :*)
-           (sql.helpers/from :customer)
-           (sql.helpers/where [:= :uuid id])
-           sql/format))
+(defn get-by-id [id db ctx]
+  (-> (sql.helpers/select :*)
+      (sql.helpers/from :customer)
+      (sql.helpers/where [:= :uuid id])
+      sql/format
+      ((db.utils/build-execute-with-ctx db ctx))
       first))
 
-(defn get-by-username [username db]
-  (-> (database/execute
-       db
-       (-> (sql.helpers/select :*)
-           (sql.helpers/from :customer)
-           (sql.helpers/where [:= :username username])
-           sql/format))
+(defn get-by-username [username db ctx]
+  (-> (sql.helpers/select :*)
+      (sql.helpers/from :customer)
+      (sql.helpers/where [:= :username username])
+      sql/format
+      ((db.utils/build-execute-with-ctx db ctx))
       first))
