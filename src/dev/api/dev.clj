ca42238df114e12c9603b3b0213d11662b438053
Josue Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-07-01 18:51:41 -0300

    feat: diff personal from org mock-types on retrieval

---
+++ src/dev/api/dev.clj
@@ -0,0 +1,54 @@
+(ns dev.api.dev
+  (:require [back.api.ports.workers :as p.workers]
+            [back.api.routes :as routes]
+            [com.moclojer.components.core :as components]
+            [com.moclojer.components.publisher :as publisher]
+            [com.stuartsierra.component :as component]
+            [dev.utils :as utils])
+  (:gen-class))
+
+;; This namespace is used for development purposes only;
+;; it is not included in the uberjar.
+(def sys-atom (atom nil))
+
+(defn build-system-map []
+  (component/system-map
+   :config (components/new-config "back/config.edn")
+   :sentry (components/new-sentry-mock)
+   :http (components/new-http)
+   :router (components/new-router routes/routes)
+   :database (component/using (components/new-database) [:config])
+   :publisher (component/using (components/new-publisher
+                                #_[{:qname "mocks.verify"
+                                    :event {}
+                                    ;; every other minute
+                                    :delay 120000}
+                                   {:qname "yml.verify"
+                                    :event {}
+                                    ;; every 5 minutes
+                                    :delay 300000}])
+                               [:config])
+   :webserver (component/using (components/new-webserver)
+                               [:config :http :router :database :publisher])
+   :workers (component/using (components/new-consumer p.workers/workers false)
+                             [:config :database :publisher])))
+
+(comment
+  (def c (component/start (components/new-config "back/config.edn")))
+  c
+  ;;
+  )
+
+(comment
+  ;; init
+  (utils/start-system-dev! sys-atom (build-system-map))
+
+  (publisher/publish! (:publisher @sys-atom) "yml.unified.verification.fired" {})
+  (publisher/publish! (:publisher @sys-atom) "domains.verification.fired" {})
+
+  ;; iterate
+  (utils/stop-system-dev! sys-atom false)
+  ;; re-eval file then
+  (utils/start-system-dev! sys-atom (build-system-map) false)
+  ;; finish
+  (utils/stop-system-dev! sys-atom))
