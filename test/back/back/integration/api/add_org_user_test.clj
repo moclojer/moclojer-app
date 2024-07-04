6fcd0bb1e3d5b95a9cd1cdea8a7d5f09e3584361
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-04 20:52:14 -0300

    test: add org user test

--- test/back/back/integration/api/add_org_user_test.clj
+++ test/back/back/integration/api/add_org_user_test.clj
@@ -1,5 +1,6 @@
-(ns back.integration.api.create-org-test
+(ns back.integration.api.add-org-user-test
   (:require [back.api.db.customers :as db.customers]
+            [back.api.db.orgs :as db.orgs]
             [back.api.routes :as routes]
             [back.integration.api.helpers :as helpers]
             [back.integration.components.utils :as utils]
@@ -22,28 +23,27 @@
     :webserver (component/using (components/new-webserver)
                                 [:config :http :router :database :publisher]))))
 
-(defn fcreate-org
-  [user]
+(defn fcreate-org-user
+  [org user]
   (flow
-    "should create an organization"
-    [:let [org {:orgname "test-123"}
-           req {:headers {"authorization"
+    "should add an user to an organization"
+    [:let [req {:headers {"authorization"
                           (str "Bearer "
                                "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkY2ZmNGMwNi0"
                                "xYzllLTRhYmItYTQ5Yi00MzhlMTg2OWVjNWIifQ.Gd42M"
                                "G5EQCVvQwsvlhRQWHuEr-BBo4GB7Pd9di8w_No")}
-                :uri "/orgs"
+                :uri (str "/orgs/" (:org/id org) "/users")
                 :method :post
-                :body {:org org}}
+                :body {:user-id (:customer/uuid user)}}
            resp (helpers/request! req)]]
     (match?
      (matchers/embeds
-      {:status 201
-       :body {:org (assoc org :users [{:uuid (str (:customer/uuid user))}])}})
+      {:status 200
+       :body {:users [{:uuid (str (:customer/uuid user))}]}})
      resp)))
 
 (defflow
-  flow-create-org
+  flow-add-org-user
   {:init (utils/start-system! create-and-start-components)
    :cleanup utils/stop-system!
    :fail-fast? true}
@@ -52,7 +52,12 @@
     [:let [user #:customer{:uuid #uuid "cd989358-af38-4a2f-a1a1-88096aa425a7"
                            :email "test@gmail.com"
                            :username "foobar123"
-                           :external-uuid #uuid "dcff4c06-1c9e-4abb-a49b-438e1869ec5b"}]
+                           :external-uuid #uuid "dcff4c06-1c9e-4abb-a49b-438e1869ec5b"}
+           org #:org{:id #uuid "d50f35a1-cba2-4d62-88aa-0c3df71c410c"
+                     :slug "my-org-d50f35a1"
+                     :orgname "my-org"}]
      {:keys [database]} (state-flow.api/get-state)]
     (state/invoke #(db.customers/insert! user database))
-    (fcreate-org user)))
+    (state/invoke #(db.orgs/insert! org database))
+
+    (fcreate-org-user org user)))
