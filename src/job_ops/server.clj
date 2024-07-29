8ac3525c4bea5e6d5ed6fc153ab542e0f421f69a
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-29 09:45:47 -0300

    feat: mv triggers back to `api`

--- src/job_ops/server.clj
+++ src/job_ops/server.clj
@@ -12,16 +12,7 @@
    :config (components/new-config "back/config.edn")
    :sentry (component/using (components/new-sentry) [:config])
    :mq (component/using
-        (components/new-mq workers
-                           [{:channel "domains.verification.fired"
-                             :event {}
-                                ;; every 2 minutes
-                             :sleep 120000}
-                            {:channel "yml.unified.verification.fired"
-                             :event {}
-                                ;; every 5 minutes
-                             :sleep 300000}]
-                           false)
+        (components/new-mq workers false)
         [:config :sentry])))
 
 (defn start-system! [system-map]
