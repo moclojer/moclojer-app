f5059d3d716795b07ad0e382a2bf9b3508b3bf71
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-07-29 17:12:10 -0300

    fix: first login rendering issue

--- src/front/app/auth/views.cljs
+++ src/front/app/auth/views.cljs
@@ -95,8 +95,8 @@
   (let [username (refx/use-sub [:app.auth/username-to-save])
         available? (refx/use-sub [:app.auth/is-username-available?])]
     (d/div {:class-name "flex flex-col justify-center items-center px-6 pt-8 mx-auto md:h-screen pt:mt-0 dark:bg-gray-900"}
-           (d/div {:class-name (str "justify-center items-center w-full bg-white rounded-lg shadow lg:flex md:mt-0"
-                                    "lg:max-w-screen-lg 2xl:max:max-w-screen-lg xl:p-0 dark:bg-gray-800")}
+           (d/div {:class-name (str "flex justify-center items-center w-full bg-white rounded-lg shadow lg:flex"
+                                    "md:mt-0 lg:max-w-screen-lg 2xl:max:max-w-screen-lg xl:p-0 dark:bg-gray-800")}
                   (d/div {:class-name "hidden w-2/3 lg:flex"}
                          (d/img {:src "/images/authentication/login.svg" :class-name "rounded-l-lg"}))
                   (if sent?
@@ -108,48 +108,48 @@
                              (d/div ($ loading-spinner)
                                     "loading ...")
                              (d/div
-                              (d/h2 {:class-name "text-2xl font-bold text-gray-900 lg:text-3xl dark:text-white"}
-                                    "welcome, enter your username.")
-                              (d/form {:disabled loading?
-                                       :on-submit (fn [e]
-                                                    (.preventDefault e)
-                                                    (when (and (not-empty username) available?)
-                                                      (refx/dispatch [:app.auth/save-username username])))
-                                       :class-name "mt-8 space-y-6"}
-                                      (d/div
-                                       (d/label
-                                        {:for "username"
-                                         :class-name "block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
-                                        "Your username")
-                                       (d/input
-                                        {:for "username"
-                                         :placeholder "username"
-                                         :name "username"
-                                         :value username
-                                         :required true
-                                         :on-change (fn [e]
-                                                      (.preventDefault e)
-                                                      (refx/dispatch-sync [:app.auth/username-available?
-                                                                           (.. e -target -value)]))
+                               (d/h2 {:class-name "text-2xl font-bold text-gray-900 lg:text-3xl dark:text-white"}
+                                     "welcome, enter your username.")
+                               (d/form {:disabled loading?
+                                        :on-submit (fn [e]
+                                                     (.preventDefault e)
+                                                     (when (and (not-empty username) available?)
+                                                       (refx/dispatch [:app.auth/save-username username])))
+                                        :class-name "mt-8 space-y-6"}
+                                       (d/div
+                                         (d/label
+                                           {:for "username"
+                                            :class-name "block mb-2 text-sm font-medium text-gray-900 dark:text-white"}
+                                           "Your username")
+                                         (d/input
+                                           {:for "username"
+                                            :placeholder "username"
+                                            :name "username"
+                                            :value username
+                                            :required true
+                                            :on-change (fn [e]
+                                                         (.preventDefault e)
+                                                         (refx/dispatch-sync [:app.auth/username-available?
+                                                                              (.. e -target -value)]))
 
-                                         :class-name "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"}))
-                                      (d/div {:class-name "text-sm font-medium text-gray-500 dark:text-gray-400"}
-                                             (d/b {:class-name "text-gray-900"} "*.<username>.")
-                                             (d/span {:class-name "text-gray-500"} "moclojer.com"))
+                                            :class-name "bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"}))
+                                       (d/div {:class-name "text-sm font-medium text-gray-500 dark:text-gray-400"}
+                                              (d/b {:class-name "text-gray-900"} "*.<username>.")
+                                              (d/span {:class-name "text-gray-500"} "moclojer.com"))
 
-                                      ($ button
-                                         {:class-name (if available? "" " cursor-not-allowed")
-                                          :type "submit"
-                                          :disabled (or loading? (not available?))
-                                          :variant "solid-blue"
-                                          :color (if available?
-                                                   "pink"
-                                                   "grey")}
-                                         (if loading?
-                                           (d/span {:class-name "inline-flex"}
-                                                   ($ loading-spinner {})
-                                                   "loading...")
-                                           (d/span "Save"))))))))))))
+                                       ($ button
+                                          {:class-name (if available? "" " cursor-not-allowed")
+                                           :type "submit"
+                                           :disabled (or loading? (not available?))
+                                           :variant "solid-blue"
+                                           :color (if available?
+                                                    "pink"
+                                                    "grey")}
+                                          (if loading?
+                                            (d/span {:class-name "inline-flex"}
+                                                    ($ loading-spinner {})
+                                                    "loading...")
+                                            (d/span "Save"))))))))))))
 
 
 
@@ -158,16 +158,16 @@
         sent? (refx/use-sub [:app.auth/username-sent])
         [error error-res] (refx/use-sub [:app.auth/login-error])]
     (hooks/use-effect
-     [sent?]
-     (when sent?
-       (rfe/push-state :app.core/dashboard)))
+      [sent?]
+      (when sent?
+        (rfe/push-state :app.core/dashboard)))
 
     (d/div
-     ($ view-container
-        ($ first-login {:sent? sent?
-                        :loading? loading?
-                        :error error
-                        :error-res error-res})))))
+      ($ view-container
+         ($ first-login {:sent? sent?
+                         :loading? loading?
+                         :error error
+                         :error-res error-res})))))
 
 (defnc login-view []
   (let [loading? (refx/use-sub [:app.auth/login-loading])
