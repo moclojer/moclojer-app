c1573ba78ee32526bb2bc1e5e3e1a0a6a1a1a2bf
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-08-19 16:32:28 -0300

    feat: update on mock changes

--- resources/back/migrations/20240812181854-mock-history.up.sql
+++ resources/back/migrations/20240812181854-mock-history.up.sql
@@ -9,3 +9,21 @@ CREATE TABLE IF NOT EXISTS mock_history (
     REFERENCES mock(id)
     ON DELETE CASCADE
 );
+
+--;;
+
+CREATE OR REPLACE FUNCTION log_mock_archive()
+RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO mock_history (id, mock_id, content, created_at)
+  VALUES (uuid_generate_v4(), NEW.id, NEW.content, CURRENT_TIMESTAMP);
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--;;
+
+CREATE TRIGGER trigger_mock_archive
+AFTER INSERT OR UPDATE ON mock
+FOR EACH ROW
+EXECUTE FUNCTION log_mock_archive();
