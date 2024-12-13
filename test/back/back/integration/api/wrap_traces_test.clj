39c0ffdec307aa3e34045a2df68e67dec5c4e43f
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-12-13 11:36:38 -0300

    feat: simple test for tracing ns

--- test/back/back/integration/api/wrap_traces_test.clj
+++ test/back/back/integration/api/wrap_traces_test.clj
@@ -1,7 +1,7 @@
 (ns back.integration.api.wrap-traces-test
   (:require
    [back.api.routes :as routes]
-   [back.api.controllers.mocks :as mocks]
+   [dev.utils :as u]
    [back.integration.components.utils :as utils]
    [com.moclojer.components.core :as components]
    [com.stuartsierra.component :as component]
@@ -22,21 +22,22 @@
     :webserver (component/using (components/new-webserver)
                                 [:config :http :router :database :mq]))))
 (defn- save-local-fns []
-  (let [a-ns (mocks/get-allowed-ns)
+  (let [a-ns (u/get-allowed-ns)
         m (atom [])]
     (doseq [d-ns a-ns]
-      (doseq [[func _] (ns-publics d-ns)]  
-        (swap! m conj (str func))))       
-    @m))  
+      (doseq [[func _] (ns-publics d-ns)]
+        (swap! m conj (str d-ns "/" func))))
+    @m))
 
 (defflow
-  flow-wrap-all-ns-functions
+  fwrap-all-ns-functions
   {:init (utils/start-system! create-and-start-components)
    :cleanup utils/stop-system!
    :fail-fast? true}
 
   (flow
-    "will try to wrap all desired ns functions into a trace container"
+    "will try to wrap all desired ns functions into a trace container 
+    and will compare them just by name"
     (let [local-fns (save-local-fns)
-          expected-fns (mocks/trace-all-ns-test)]
+          expected-fns (u/trace-all-ns)]
       (match? (matchers/equals local-fns) expected-fns))))
