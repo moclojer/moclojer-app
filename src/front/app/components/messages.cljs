fe66bcf1e232438eea4c92fa1db93e4c77f6b629
Felipe-gsilva <felipe.gsilva@protonmail.com>
2025-03-08 09:26:01 -0300

    docs: message on git sync

--- src/front/app/components/messages.cljs
+++ src/front/app/components/messages.cljs
@@ -39,11 +39,9 @@
           (d/div {:class "flex items-center mb-4"}
                  (d/div {:class "flex-shrink-0"}
                         ($ pfp
-                           {:style (props :image-style)
-                            :pfp-url (if (= true (props :pfp-loading?))
-                                       "/images/default-pfp.png"
-                                       (props :image))
-
+                           {:src (if (= true (props :pfp-loading?))
+                                   "/images/default-pfp.png"
+                                   (props :image))
                             :pfp-loading? false}))
                  (d/div {:class "ml-4"}
                         (d/p (props :author))
@@ -60,19 +58,22 @@
 
 (defnc blogpost-message
   [{:keys [children] :as props}]
-  (d/div {:class (str "p-4 bg-gray-200 block sm:flex items-center justify-left border-b mr-8 py-8"
-                      "border-gray-200 lg:mt-1.5 dark:bg-gray-800 dark:border-gray-700")}
-         (d/div {:class "flex items-center mb-4"}
+  (d/div {:class (str "w-full p-4 bg-gray-100 block flex flex-row sm:flex-col items-center justify-left border-b py-8"
+                      "border-gray-200 lg:mt-1.5 space-y-2")}
+         (d/div {:class "flex items-center justify-left w-full"}
                 (d/div {:class "flex-shrink-0"}
-                       ($ pfp-img
-                          {:style (props :image-style)
-                           :pfp-loading? false
-                           :pfp-url (props :image)}))
+                       ($ pfp
+                          {:pfp-loading? false
+                           :src (props :image)}))
                 (d/div {:class "ml-4"}
-                       (d/div {:class "mltext-sm text-gray-500 text-normal dark:text-gray-400"}
+                       (d/div {:class "text-sm text-gray-500 text-normal dark:text-gray-400"}
                               (d/p (props :author))
                               (d/i (props :email)))))
+         (d/div {:class "flex items-center justify-left w-full "}
+                (d/a {:class "text-blue-500 " :href (props :link) :target "_blank"} (str/join "/" (take-last 2 (str/split (props :link) #"/")))))
+         (d/div {:class "flex items-center justify-left w-full "}
+                ($ text {:message (seq (props :message)) :index 0}))
 
-         (d/div {:class "ml-4"}
-                (d/a {:class "text-blue-500 " :href (props :link) :target "_blank"} (props :link)))))
+         (d/div {:class "flex items-center justify-left w-full "}
+                children)))
 
--- src/front/app/dashboard/views.cljs
+++ src/front/app/dashboard/views.cljs
@@ -1,18 +1,40 @@
 (ns front.app.dashboard.views
   (:require
+   [mockingbird.components.button :refer [button]]
    [mockingbird.components.message :refer [message]]
-   [front.app.dashboard.base :as base]
    [mockingbird.lib :refer-macros [defnc]]
+   [front.app.components.svg :as svg]
+   [front.app.dashboard.base :as base]
+   [front.app.components.messages :refer [blogpost-message]]
    [helix.core :refer [$]]
-   [helix.dom :as d]))
+   [helix.dom :as d]
+   [refx.alpha :as refx]))
 
 (defnc welcome []
-   ($ base/index
-      (d/main
-       ($ message
-          {:title "Welcome to moclojer beta"
-           :email "avelino@moclojer.com"
-           :author "Avelino"
-           :image "/images/users/avatar.png"
-           :image-style "rounded"
-           :message ["I'm happy to have you here, we are launching the SaaS version of moclojer, seeking to make it simple for you to put a mock API in the air to develop your projects." "I look forward to your feedback (this is the only way we can improve the product)."]}))))
+  ($ base/index
+     (d/main
+      ($ message
+         {:title "Welcome to moclojer beta"
+          :email "avelino@moclojer.com"
+          :author "Avelino"
+          :image "/images/users/avatar.png"
+          :image-style "rounded"
+          :message ["I'm happy to have you here, we are launching the SaaS version of moclojer, seeking to make it simple for you to put a mock API in the air to develop your projects."
+                    "I look forward to your feedback (this is the only way we can improve the product)."]})
+      (d/div {:class "pb-4"}
+             ($ blogpost-message
+                {:title (d/span {:class "flex items-center space-x-2 "} (d/p "New Moclojer Sync Feature") ($ svg/github {:class "w-6 h-6 "}))
+                 :email "team@moclojer.com"
+                 :author "Moclojer Team"
+                 :image "/images/default-pfp.png"
+                 :image-style "rounded"
+                 :link "https://github.com/moclojer/moclojer-app"
+                 :message [(d/span "We are happy to announce we are releasing a new (work in progress) feature. " (d/span {:class "font-bold"} "Github Synchronization"))
+                           "Take a look at the documentation and help us improve this project by giving your feedback"]}
+                ($ button {:class "text-gray-500 px-4 py-2 border border-gray-500 rounded-lg hover:bg-gray-200 flex space-x-2 items-center justify-center transition duration-75 "
+                           :theme :mockingbird
+                           :type :icon
+                           :size :md
+                           :on-click #(refx/dispatch-sync [:app.dashboard/toggle-git-docs])}
+                   (d/p {:class "text-center"} "docs")
+                   ($ svg/book {:class "w-6 h-6 "})))))))
